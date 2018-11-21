2107806725eccb84b42f576a451755eac7ea5ae1
ande4694 <knudsen879@hotmail.com>
2018-11-21 10:02:59 +0100

    fixet

--- src/main/java/snsinternaltransfer/sns/service/LoginService.java
+++ src/main/java/snsinternaltransfer/sns/service/LoginService.java
@@ -1,38 +1,40 @@
-package snsinternaltransfer.sns.service;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import snsinternaltransfer.sns.models.User;
-import snsinternaltransfer.sns.repo.UserRepo;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-@Service
-public class LoginService {
-
-    @Autowired
-    UserRepo userRepo;
-    BCryptPasswordEncoder encoder;
-
-
-    public User login(String username, String password){
-
-        String hashedAndSaltedPassword = encoder.encode(password+userRepo.getSaltViaUsername(username));
-
-        if (hashedAndSaltedPassword.equals(userRepo.getPasswordViaUsername(username))){
-
-            userRepo.getUserViaUsername(username).setLoggedIn(true);
-
-            return userRepo.getUserViaUsername(username);
-
-        }
-        else return null;
-    }
-
-    // den her er til for at vi kan lave og teste passwords
-    public static void main(String[] args) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-
-    }
-}
+///*
+//package snsinternaltransfer.sns.service;
+//
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import snsinternaltransfer.sns.models.User;
+//import snsinternaltransfer.sns.repo.UserRepo;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+//@Service
+//public class LoginService {
+//
+//    @Autowired
+//    UserRepo userRepo;
+//    BCryptPasswordEncoder encoder;
+//
+//
+//    public User login(String username, String password){
+//
+//        String hashedAndSaltedPassword = encoder.encode(password+userRepo.getSaltViaUsername(username));
+//
+//        if (hashedAndSaltedPassword.equals(userRepo.getPasswordViaUsername(username))){
+//
+//            userRepo.getUserViaUsername(username).setLoggedIn(true);
+//
+//            return userRepo.getUserViaUsername(username);
+//
+//        }
+//        else return null;
+//    }
+//
+//    // den her er til for at vi kan lave og teste passwords
+//    public static void main(String[] args) {
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//
+//
+//    }
+//}
+//*/
